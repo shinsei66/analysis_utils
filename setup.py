a0ed73e3fe6c9a92aabe081409101c5922214c68
shinsei66 <48095788+shinsei66@users.noreply.github.com>
2021-02-28 22:22:40 +0900

    built package trial 1
    built the package for the first time, but still cannot import the library now.

--- setup.py
+++ setup.py
@@ -2,25 +2,26 @@ from setuptools import setup, find_packages
 
 
 install_requires = [
-    'pandas'
+    'pandas', 'pep517>=0.9'
 ]
 
 
 
-console_scripts = [
+# console_scripts = [
     
-]
+# ]
 
 
 setup(
     name='analysis_utils',
     description='Python Data Analysis Utilities',
     author='shinsei66',
-    # author_email='XXS@XXXX.net',
+    author_email='XXS@XXXX.net',
     url='https://github.com/shinsei66',
     version='0.0.0',
-    packages=find_packages,
+    packages=find_packages(), # find_packages で()が抜けるとエラーになる
     install_requires=install_requires,
     license='MIT',
-    entry_points={'console_scripts': console_scripts},
+    # entry_points={'console_scripts': console_scripts},
+    python_requires=">=3.7",
 )
